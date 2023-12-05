aa6b8341a108ded4cba1557bac792f362793727e
Nikita Dubinin <nik.dubinin.01@mail.ru>
2023-12-05 22:46:33 +0300

    added sendData & getData functions

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 "use strict";
+
+const getData = () => {
+  fetch("db.json")
+    .then((response) => response.json())
+    .then((data) => {
+      sendData(
+        "https://jsonplaceholder.typicode.com/posts",
+        JSON.stringify(data)
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data);
+        })
+        .catch((error) => console.log(error));
+    })
+    .catch((error) => console.log(error));
+};
+
+const sendData = (url, user) => {
+  return fetch(url, {
+    method: "POST",
+    body: user,
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+  });
+};
+
+getData();
