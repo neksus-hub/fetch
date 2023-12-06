74d6e19f0ad8856bfc89d31c927b42c4918b5c57
Nikita Dubinin <nik.dubinin.01@mail.ru>
2023-12-06 21:34:42 +0300

    sending data using the XMLHttpRequest method

--- script.js
+++ script.js
@@ -7,24 +7,25 @@ const getData = () => {
       sendData(
         "https://jsonplaceholder.typicode.com/posts",
         JSON.stringify(data)
-      )
-        .then((response) => response.json())
-        .then((data) => {
-          console.log(data);
-        })
-        .catch((error) => console.log(error));
+      );
     })
     .catch((error) => console.log(error));
 };
 
 const sendData = (url, user) => {
-  return fetch(url, {
-    method: "POST",
-    body: user,
-    headers: {
-      "Content-type": "application/json; charset=UTF-8",
-    },
-  });
+  let xhr = new XMLHttpRequest();
+
+  xhr.open("POST", url, true);
+
+  xhr.setRequestHeader("Content-Type", "application/json");
+
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      console.log(xhr.response);
+    }
+  };
+
+  xhr.send(user);
 };
 
 getData();
